8de3c6c224ab399d37643f6b8900efadb1e34987
wouterDumon <wouter.dumon@student.howest.be>
2015-04-25 18:30:20 +0200

    Google Maps geintegreerd
    TODO:
    -Kijken welke opties allemaal voor op de map ( markers,...)
    -JSON  inladen
    -JSON weergeven op de map
    -GUI maken met opties voor map weer te geven
    -Map laten veranderen door de opties
    -3de fragment aan maken
    -DETAILS van geselecteerde marker op 3de fragment tonen
    -navigatie tussen de 3 fragmenten maken

--- DESProject/app/src/main/java/nmct/howest/be/desproject/MainFragment.java
+++ DESProject/app/src/main/java/nmct/howest/be/desproject/MainFragment.java
@@ -10,14 +10,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link MainFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link MainFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class MainFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,14 +23,7 @@ public class MainFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MainFragment.
-     */
+
     // TODO: Rename and change types and number of parameters
     public static MainFragment newInstance(String param1, String param2) {
         MainFragment fragment = new MainFragment();
